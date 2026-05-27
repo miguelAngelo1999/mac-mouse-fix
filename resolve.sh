cb475cc73c5bef8351bff2d5d84b52640bb9c578
Virgoh <nimishclho@gmail.com>
2026-05-27 13:22:00 -0300

    Clean up resolve scripts

--- resolve.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for f in $(git diff --name-only --diff-filter=U); do
-    sed -i '' '/^<<<<<<< HEAD$/d' "$f"
-    sed -i '' '/^=======$/d' "$f"
-    sed -i '' '/^>>>>>.*/d' "$f"
-    git add "$f"
-done
-git cherry-pick --continue --no-edit
